8f12133f596b172ca19ad30cd7ad1403a5cb5317
Jacob Ottesen <offar@hotmail.com>
2013-08-09 15:10:40 +0200

    MVVM

---
+++ desktop/CodeProject.OutlookNavi/Sensor/SensorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeProject.OutlookNavi
+{
+    class SensorViewModel : ObservableObject, IPageViewModel
+    {
+        public string Name
+        {
+            get
+            {
+                return "Sensor Page";
+            }
+        }
+    }
+}
---
+++ desktop/CodeProject.OutlookNavi/Sensor/SensorViewModel.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CodeProject.OutlookNavi.Sensor
+{
+    /// <summary>
+    /// Interaction logic for Sensor.xaml
+    /// </summary>
+    public partial class Sensor : Window
+    {
+        public Sensor()
+        {
+            InitializeComponent();
+        }
+    }
+}
