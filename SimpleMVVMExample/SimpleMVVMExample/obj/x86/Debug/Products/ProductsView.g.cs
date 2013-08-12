b93ebfda51fb91ab2451377c7ec65cf0e47d801f
Jacob Home <offar@hotmail.com>
2013-08-12 21:35:17 +0200

    Binding commit

--- SimpleMVVMExample/SimpleMVVMExample/obj/x86/Debug/Products/ProductsView.g.cs
+++ SimpleMVVMExample/SimpleMVVMExample/obj/x86/Debug/Products/ProductsView.g.cs
@@ -9,6 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using Microsoft.Windows.Themes;
 using SimpleMVVMExample;
 using System;
 using System.Diagnostics;
